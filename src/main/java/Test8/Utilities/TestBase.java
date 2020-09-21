2132d6b28e9dbe0c8cbcfcdf923431f29133b1b6
Fatih <yalcinfatihtr@gmail.com>
2020-09-20 20:32:43 -0400

    first commit

---
+++ src/main/java/Test8/Utilities/TestBase.java
@@ -0,0 +1,27 @@
+package Test8.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+
+
+    }
+
+
+}
---
+++ src/test/java/SearchUsers.java
@@ -0,0 +1,24 @@
+import Test8.Utilities.TestBase;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SearchUsers extends TestBase {
+
+
+    @Test
+    public void search(){
+        driver.get("http://www.kaolapalace-qa-environment2.com/");
+
+
+
+
+
+    }
+
+
+
+
+
+}
