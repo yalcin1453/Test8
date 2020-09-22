7b941ebdf86d512fc788aa2761c17d40b3def5ea
Fatih <yalcinfatihtr@gmail.com>
2020-09-22 11:24:35 -0400

    yalcin commit

--- src/test/java/SearchUsers.java
+++ src/test/java/SearchUsers.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class SearchUsers extends TestBase {
@@ -21,8 +23,13 @@ public class SearchUsers extends TestBase {
     @Test
     public void userSearch(){
         login();
-        driver.findElement(By.id("name"));
-        driver.findElement();
+        WebElement email= driver.findElement(By.xpath("(//input[@class='form-control form-filter input-sm'])[3]"));
+        email.sendKeys("fatih721@gmail");
+        WebDriverWait wait= new WebDriverWait(driver,20);
+        WebElement search = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//i[@class='fa fa-search'])[1]")));
+        search.click();
+
+       
         
 
 
